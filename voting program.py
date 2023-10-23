28a317e71081d9457c44b55b24b9d3cc5621ec74
CHELSEA SHIM <90699046+mukabwashim@users.noreply.github.com>
2023-10-24 00:59:23 +0300

    Add files via upload

---
+++ voting program.py
@@ -0,0 +1,8 @@
+#voting process program
+age =int(input("enter age"))
+citizenship =(input("enter citizenship"))
+if age>=18 and citizenship =="kenyan":
+    print("eligible to vote")
+else:
+    print("not eligible for voting ")
+    
